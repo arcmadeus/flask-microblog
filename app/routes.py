25cdc53a9f97d17cf18679b5a99dd21ede98337e
Arc <timmarticio@gmail.com>
2025-07-07 22:51:48 +0800

    Chapter 2, making index.html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 # Importing routes (It handles different URLs)
 @app.route('/') # Decorators are used as callbacks for certain events
 @app.route('/index')
 def index():
-  return "Hello, World!"
+	user = {'username': 'Marco'}
+	return render_template('index.html', title='Home', user=user)
