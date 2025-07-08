53ad029bcf8e79e9fe061023166006034b5960b2
Arc <timmarticio@gmail.com>
2025-07-08 18:16:11 +0800

    Chapter 5, validation of user accounts

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,17 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from urllib.parse import urlsplit
 
 # Importing routes (It handles different URLs)
 @app.route('/') # Decorators are used as callbacks for certain events
 @app.route('/index')
+@login_required
 def index():
-	user = {'username': 'Marco'}
 	posts = [
 		{
 			'author': {'username': 'Yushin'},
@@ -22,7 +23,7 @@ def index():
 		},
 	]
 
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home Page', posts=posts)
 
 #GET requests information to the client, POST requests used to submit forms
 @app.route('/login', methods=['GET', 'POST']) # Accepts both GET and POST request
@@ -43,8 +44,11 @@ def login():
 			return redirect(url_for('login'))
 		
 		# Redirects the newly-logged in user to the index page.
-		login_user(user, remember_me=form.remember_me.data)
-		return redirect(url_for('index')) #Redirect the user to the index page of the application.
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or urlsplit(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(url_for(next_page)) #Redirect the user to the index page of the application.
 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
