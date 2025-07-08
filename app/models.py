337f8c17f29999c244b27d014456b2fc940e73f1
Arc <timmarticio@gmail.com>
2025-07-08 17:36:48 +0800

    Chapter 5, authenticated user's login and logout.

--- app/models.py
+++ app/models.py
@@ -2,9 +2,12 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key = True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -13,6 +16,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     """
@@ -29,3 +38,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Posts {self.body}'
+
+# Because Flask-Login knows nothing about databases, it needs the application's help in loading a user.
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 # Importing routes (It handles different URLs)
 @app.route('/') # Decorators are used as callbacks for certain events
@@ -23,8 +27,27 @@ def index():
 #GET requests information to the client, POST requests used to submit forms
 @app.route('/login', methods=['GET', 'POST']) # Accepts both GET and POST request
 def login():
+
+	# Returns the user in the index page IF the account is logged in.
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
 	form = LoginForm()
 	if form.validate_on_submit(): # Does all the form processing work
-		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data)) # flash() function is a useful way to show a message to the user. 
+		user = db.session.scalar( # Load the user from the database
+			sa.select(User).where(User.username == form.username.data)
+		) # Finding the user's username and since it will return 1 or 0, use SCALAR if it exists or not
+
+		if user is None or not user.check_password(form.password.data): # Checks the password
+			flash('Invalid username or password.')
+			return redirect(url_for('login'))
+		
+		# Redirects the newly-logged in user to the index page.
+		login_user(user, remember_me=form.remember_me.data)
 		return redirect(url_for('index')) #Redirect the user to the index page of the application.
 	return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
