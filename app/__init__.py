4377ba6b112ab33b2c3a201ea91bbeff11c1e355
Arc <timmarticio@gmail.com>
2025-07-08 08:13:10 +0800

    Chapter 4, creation of database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) # Setting a name for this module
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # the app variable is an instance of Flask
+from app import routes, models # the app variable is an instance of Flask
 
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # Uses to protect against Cross-site Request Forgery
     SECRET_KEY = os.environ.get('SECRET_KEY') or "timados_unidos_united"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
