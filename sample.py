23276e5fd907dfa8db15e6170008f32f657f6524
Arc <timmarticio@gmail.com>
2025-07-08 21:54:22 +0800

    Chapter 5: User Logins [Completed]

--- sample.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from app.models import User, Post
-import sqlalchemy as sa
-
-query = sa.select(User)
-users = db.session.scalars(query).all()
-for u in users:
-    print(u.id, u.username)
