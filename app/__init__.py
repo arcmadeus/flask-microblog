827c5b6b03309cd5adfd9847c7166b98f953f391
Marco <timmarticio@gmail.com>
2025-07-31 14:35:48 +0800

    Chapter 15: A Better Application Structure [Completed]

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,18 @@ def create_app(config_class=Config):
     moment.init_app(app)
     babel.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.cli import bp as cli_bp
+    app.register_blueprint(cli_bp)
+
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
@@ -63,14 +75,7 @@ def create_app(config_class=Config):
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
 
-    from app.errors import bp as errors_bp
-    app.register_blueprint(errors_bp)
-
-    from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-
-    from app.cli import bp as cli_bp
-    app.register_blueprint(cli_bp)
+    
 
     return app
 
